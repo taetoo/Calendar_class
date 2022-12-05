57a48508bc218abf69518bf1c5fff530b0d9ca70
taetoo <rlaougs5@naver.com>
2022-12-05 16:07:59 +0900

    Reat: 요일 메서드 추가

--- src/main/java/com/example/calendar/CalendarTest.java
+++ src/main/java/com/example/calendar/CalendarTest.java
@@ -9,31 +9,82 @@ import java.util.Scanner;
 @Slf4j
 public class CalendarTest {
     public static void main(String[] args) {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM월 dd일");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 M월 d일 ");
+        SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy년 M월 d일 ");
+
 
         // 현재
         Calendar cal = Calendar.getInstance();
         int year = cal.get(Calendar.YEAR);
         int month = cal.get(Calendar.MONTH);
         int date = cal.get(Calendar.DATE);
+        int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
+
+        String korDayOfWeek = "";
+        korDayOfWeek = getYoil(korDayOfWeek, dayOfWeek);
 
         // 해당월의 총 일수
         int daysOfMonth = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
-        System.out.println("현재: " + year + "년 " + (month + 1) + "월 " + date + "일 | 총 일수 " + daysOfMonth + "일");
-        System.out.println("사용일수: " + date + " | 남은 일수: " + (daysOfMonth - date));
+        System.out.println("= 중도입주 일할계산 Test =\n");
+        System.out.println("현재: " + year + "년 " + (month + 1) + "월 " + date + "일 " + korDayOfWeek +"요일 | 총 일수 " + daysOfMonth + "일");
+        System.out.println("거주 일수: " + date + " | 남은 일수: " + (daysOfMonth - date) +"\n");
 
-        System.out.println("================================");
+
+        System.out.println("================================\n");
 
 
         // 윤달있는 년 월
-        cal.set(2024,2-1, 9);
+        cal.set(2020,2-1, 2);
 
         // 해당 월의 총 일수
         int daysOfMonth2 = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
         int yundalDate = cal.get(Calendar.DATE);
-        System.out.println("윤달: " + sdf.format(cal.getTime()) + " | 총 일수: " + daysOfMonth2);
-        System.out.println("사용 일수: " + yundalDate + " | 남은 일수: " + (daysOfMonth2 - yundalDate) + "일");
+        int dayOfWeek2 = cal.get(Calendar.DAY_OF_WEEK);
+        korDayOfWeek = getYoil(korDayOfWeek, dayOfWeek2);
 
+        System.out.println("윤달: " + sdf.format(cal.getTime()) + korDayOfWeek + "요일 | 총 일수: " + daysOfMonth2);
+        System.out.println("거주 일수: " + yundalDate + " | 남은 일수: " + (daysOfMonth2 - yundalDate) + "일");
+
+
+
+        // Notyet Test
+        System.out.println("\n================================\n");
+        System.out.println("임의 날짜 테스트");
+        cal.set(Calendar.YEAR, 2022);
+        cal.set(Calendar.MONTH, 12-1);
+        cal.set(Calendar.DATE, 24);
+        int dayOfWeek3 = cal.get(Calendar.DAY_OF_WEEK);
+        korDayOfWeek = getYoil(korDayOfWeek, dayOfWeek3);
+        System.out.println( sdf2.format(cal.getTime()) + korDayOfWeek + "요일");
+
+    }
+
+    private static String getYoil(String korDayOfWeek, int dayOfWeek2) {
+        switch (dayOfWeek2){
+            case 1:
+                korDayOfWeek = "일";
+                break;
+            case 2:
+                korDayOfWeek = "월";
+                break;
+            case 3:
+                korDayOfWeek = "화";
+                break;
+            case 4:
+                korDayOfWeek = "수";
+                break;
+            case 5:
+                korDayOfWeek = "목";
+                break;
+            case 6:
+                korDayOfWeek = "금";
+                break;
+            case 7:
+                korDayOfWeek = "토";
+                break;
+
+        }
+        return korDayOfWeek;
     }
 
 
