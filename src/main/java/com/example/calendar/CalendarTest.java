9149f9cc16c64fa639807130b9a87e9bdf284e9a
taetoo <rlaougs5@naver.com>
2022-12-05 14:31:54 +0900

    Fix: 총일수 사용일수 남은일수 추가

--- src/main/java/com/example/calendar/CalendarTest.java
+++ src/main/java/com/example/calendar/CalendarTest.java
@@ -4,40 +4,35 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Scanner;
 
 @Slf4j
 public class CalendarTest {
     public static void main(String[] args) {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM월 dd일 | 총 일수 d일");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM월 dd일");
 
+        // 현재
         Calendar cal = Calendar.getInstance();
-
         int year = cal.get(Calendar.YEAR);
         int month = cal.get(Calendar.MONTH);
         int date = cal.get(Calendar.DATE);
-        int ilsu = cal.get(Calendar.DAY_OF_MONTH);
 
-        // 현재
-        System.out.println("현재: " + year + "년 " + (month + 1) + "월 " + date + "일 | 총 일수 " + ilsu + "일");
+        // 해당월의 총 일수
+        int daysOfMonth = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+        System.out.println("현재: " + year + "년 " + (month + 1) + "월 " + date + "일 | 총 일수 " + daysOfMonth + "일");
+        System.out.println("사용일수: " + date + " | 남은 일수: " + (daysOfMonth - date));
 
-        // 11월
-        cal.set(Calendar.YEAR, 2022);
-        cal.set(Calendar.MONTH, 11 -1);
-        cal.set(Calendar.DATE, 10);
-        cal.get(Calendar.DAY_OF_MONTH);
-        System.out.println("20221110 : "  + sdf.format(cal.getTime()));
+        System.out.println("================================");
 
-        // 10월
-        cal.set(2022, 10 -1, 20);
-        cal.get(Calendar.DAY_OF_MONTH);
-        System.out.println("20221010 : " + sdf.format(cal.getTime()));
 
-        System.out.println("");
-        System.out.println("============윤달=============");
         // 윤달있는 년 월
-        cal.set(2016, 2, 0);
-        cal.get(Calendar.DAY_OF_MONTH);
-        System.out.println(sdf.format(cal.getTime()));
+        cal.set(2024,2-1, 9);
+
+        // 해당 월의 총 일수
+        int daysOfMonth2 = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+        int yundalDate = cal.get(Calendar.DATE);
+        System.out.println("윤달: " + sdf.format(cal.getTime()) + " | 총 일수: " + daysOfMonth2);
+        System.out.println("사용 일수: " + yundalDate + " | 남은 일수: " + (daysOfMonth2 - yundalDate) + "일");
 
     }
 
